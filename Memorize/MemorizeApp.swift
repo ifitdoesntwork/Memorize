1480824a2e89bc18f0f9796bda3adfc47e3f3a0c
Denis Avdeev <davdeev@me.com>
2021-07-11 11:54:03 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Denis Avdeev on 11.07.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
