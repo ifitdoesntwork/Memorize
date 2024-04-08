5eea5432535196869d76c4858d1391c0ad6dc321
Denis Avdeev <davdeev@me.com>
2024-04-08 14:56:29 +0500

    [a2rt5] Support Six Themes

--- Memorize/Theme.swift
+++ Memorize/Theme.swift
@@ -26,4 +26,43 @@ struct Theme {
     let emoji: Emoji
     let numberOfPairs: Int
     let color: Color
+    
+    static let suggestions = [
+        Theme(
+            name: "Halloween",
+            emoji: .halloween,
+            numberOfPairs: 12,
+            color: .orange
+        ),
+        Theme(
+            name: "Sports",
+            emoji: .sports,
+            numberOfPairs: 4,
+            color: .red
+        ),
+        Theme(
+            name: "Animals",
+            emoji: .animals,
+            numberOfPairs: 8,
+            color: .green
+        ),
+        Theme(
+            name: "October",
+            emoji: .halloween,
+            numberOfPairs: 6,
+            color: .red
+        ),
+        Theme(
+            name: "Big Sports",
+            emoji: .sports,
+            numberOfPairs: 10,
+            color: .blue
+        ),
+        Theme(
+            name: "My Pets",
+            emoji: .animals,
+            numberOfPairs: 3,
+            color: .orange
+        )
+    ]
 }
