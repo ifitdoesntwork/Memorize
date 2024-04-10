6520f41d1226a958951f0383e66a516dbf5a8e71
Denis Avdeev <davdeev@me.com>
2024-04-10 14:31:38 +0500

    Recreate the Demo from Lecture 6

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -9,14 +9,13 @@ import SwiftUI
 
 struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel: EmojiMemoryGame
+    private let aspectRatio: CGFloat = 2/3
     
     var body: some View {
         VStack {
             panel
-            ScrollView {
-                cards
-                    .animation(.default, value: viewModel.cards)
-            }
+            cards
+                .animation(.default, value: viewModel.cards)
         }
         .padding()
     }
@@ -43,21 +42,15 @@ struct EmojiMemoryGameView: View {
     }
     
     var cards: some View {
-        LazyVGrid(
-            columns: [GridItem(
-                .adaptive(minimum: 85),
-                spacing: 0
-            )],
-            spacing: 0
-        ) {
-            ForEach(viewModel.cards) { card in
-                CardView(card, colors: viewModel.colors)
-                    .aspectRatio(2/3, contentMode: .fit)
-                    .padding(4)
-                    .onTapGesture {
-                        viewModel.choose(card)
-                    }
-            }
+        AspectVGrid(
+            viewModel.cards,
+            aspectRatio: aspectRatio
+        ) { card in
+            CardView(card, colors: viewModel.colors)
+                .padding(4)
+                .onTapGesture {
+                    viewModel.choose(card)
+                }
         }
     }
 }
